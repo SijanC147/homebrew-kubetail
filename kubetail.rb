27332f3bab5bd01e271b7de84c76a318ff495e2e
Sean <13831460+SijanC147@users.noreply.github.com>
2023-01-19 21:11:36 +0100

    Update kubetail.rb

--- kubetail.rb
+++ kubetail.rb
@@ -1,7 +1,7 @@
 class Kubetail < Formula
   desc "Tail logs from multiple Kubernetes pods at the same time"
   homepage "https://github.com/sijanc147/kubetail"
-  url "https://github.com/johanhaleby/kubetail/archive/1.6.17.tar.gz"
+  url "https://github.com/SijanC147/kubetail/archive/refs/tags/v1.tar.gz"
   sha256 "9dee9fbc4c472cf73666ee77e8f430e3aaea2ee3ca2f34dda15a4da9a6bad69a"
   head "https://github.com/sijanc147/kubetail.git"
 
